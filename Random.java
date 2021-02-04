c172b8a90b4da9c3ca6f4ee03b0fb3cbef90f756
Jwalant Modi <75414565+JwalantModi@users.noreply.github.com>
2021-02-04 22:24:32 +0530

    Update Random.java

--- Random.java
+++ Random.java
@@ -2,7 +2,7 @@ package Practicals;
 public class Random{
 	private static int a;
 	private static int b;
-	public static int randint(int a1, int b1) {
+	public static int randomInt(int a1, int b1) {
 		a = a1;
 		b = b1+1;
 		int n;
